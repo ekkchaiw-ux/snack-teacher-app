94b7d3f1b3a44bc34f1de37ea6f7b7332d658e1c
ktcekkchai <ekkchai.wo@gmail.com>
2026-06-07 13:58:16 +0700

    Edit web White and Back

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/snack-teacher-app/',
+  plugins: [react(), tailwindcss()],
+  base: '/snck-teacher-app/', // ⚠️ สำคัญมาก: ชื่อตรงนี้ต้องตรงกับชื่อ Repository ใน GitHub ของคุณเป๊ะๆ (สังเกตว่ามี / ปิดหัวท้าย)
 })
